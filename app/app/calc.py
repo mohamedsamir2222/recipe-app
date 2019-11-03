3854b84f48199aaacabf04fe5961c37063c1b464
AH10 <ahmed_hesham@outlook.com>
2019-11-03 19:42:59 +0200

    Add Example For Test Driven Development

---
+++ app/app/calc.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+def add(x, y):
+    """Add two numbers together and return the result"""
+
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract x from y and return result"""
+
+    return y - x
