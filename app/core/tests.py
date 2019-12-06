5f4b8577b60521fe29243678090ec62c697d2eb6
AH10 <ahmed_hesham@outlook.com>
2019-12-06 16:08:36 +0200

    Adding Recipe Rest-EndPoints

--- app/core/tests.py
+++ app/core/tests.py
@@ -143,3 +143,19 @@ class IngrediantsModelTestCase(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+
+# Testing Cases for Recipe Model ...
+
+class RecipeModelTestCase(TestCase):
+
+    def test_recipe_str(self):
+        """Test the recipe string representation"""
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title='Steak and mushroom sauce',
+            time_minutes=5,
+            price=5.00
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
