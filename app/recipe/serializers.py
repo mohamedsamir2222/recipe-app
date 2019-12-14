6e742fc99539ff2fe40107936946f4e52590e669
AH10 <ahmed_hesham@outlook.com>
2019-12-15 00:03:43 +0200

    Adding Uploading Image Feature to Recipe App

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-
 from core.models import Tag, Ingredient, Recipe
 
 
@@ -46,3 +45,12 @@ class RecipeSerializer(serializers.ModelSerializer):
 class RecipeDetailSerializer(RecipeSerializer):
     ingredients = IngredientSerializer(many=True, read_only=True)
     tags = TagSerializer(many=True, read_only=True)
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer for uploading images to recipe"""
+
+    class Meta:
+        model = Recipe
+        fields = ('id', 'image')
+        read_only_fields = ('id',)
